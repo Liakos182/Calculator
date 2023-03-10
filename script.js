d4a2fe79250b61679ea8b5ada7b35d8c15c9b6da
Ilias Liakos <iliastsouleas@gmail.com>
2023-03-10 16:44:08 +0200

    justDidSomeWorkOnIndexAndStylesAddedAfewIdsThatIwillUseInTheScriptLaterOnAlsoWroteHowIThinkItIsGoingToWorkAsCommentsInTheScript

---
+++ script.js
@@ -0,0 +1,15 @@
+/*The calculator must have these things:
+1.The numerical buttons have to trigger an event that will add a number to
+the bottom line of the screen- so the number must be a string.
+2.The comma button should just check the string if there is a comma in there
+and if there is not it should add one.
+3.The delete button must remove a character from the bottom line of the screen.
+4.The calculation buttons should take check if there is a string in the 
+bottom line of the screen.
+It should convert it to a number and push it to the second line of the screen.
+it should also check if the second line is already full do whatever math has 
+been requiered.
+5. The equlal button should delivere the result to the bottom line.After that
+if the user presses a number it should reset the calculator starting from that
+number or if the user presses a math button it should continue from ther.
+6.The clear button completly resets the calculartor
